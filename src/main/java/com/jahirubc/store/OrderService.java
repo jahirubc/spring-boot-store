a3fadba9da9b595a389b9652bd80fa45bc9f03c0
jahirubc <jahirubc@gmail.com>
2025-04-17 19:46:49 +0600

    code updated

--- src/main/java/com/jahirubc/store/OrderService.java
+++ src/main/java/com/jahirubc/store/OrderService.java
@@ -1,19 +1,30 @@
 package com.jahirubc.store;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class OrderService {
     private PaymentService paymentService;
 
 //    @Autowired
-    public OrderService(@Qualifier("stripe") PaymentService paymentService){
+    public OrderService(PaymentService paymentService){
         this.paymentService = paymentService;
+        System.out.println("OrderService created");
+    }
+    @PostConstruct
+    public void init(){
+        System.out.println("OrderService PostConstruct");
     }
 
+    @PreDestroy
+    public void cleanup(){
+        System.out.println("OrderService PreDestroy");
+    }
     public void placeOrder(){
         paymentService.processPayment(10);
     }
